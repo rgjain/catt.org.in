67d0406ab9f0206478f291d8b27292ae6eb51b46
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-05 09:28:35 +0530

    2nd day of commit

--- website/build/index.php
+++ website/build/index.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>CATT</title>
-</head>
-<body style="background: url(images/catt-logo.png) no-repeat top center">
-
+	<head>
+		<title>CATT :: Conservation Awareness Team Trust</title>
+	</head>
+	<link type="text/css" rel="stylesheet" href="assets/styles.css">
+	<body>
+		<div id="bg-slider">
+			<header>
+				<nav>
+					<a href="./" id="logo" title="Conservation Awareness Team Trust">
+						<img src="assets/images/catt-logo-150.png" alt="CATT">
+					</a>
+					<a href="">ABOUT US</a>
+					<a href="">TEAM</a>
+					<a href="">EVENTS</a>
+					<a href="">GALLERY</a>
+					<a href="">MEMBERSHIP</a>
+					<a href="">MEMBERS &amp; VOLUNTEERS</a>
+					<a href="">CONTACT US</a>
+				</nav>
+			</header>
+		</div>
 </body>
 </html>
