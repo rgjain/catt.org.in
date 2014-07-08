64d41abd6e5b50c4ee47de86dd2957581504d045
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-08 07:23:58 +0530

    Removed the vision for now

--- website/build/index.php
+++ website/build/index.php
@@ -42,7 +42,7 @@
 				
 				default:
 					include_once 'templates/banner.php';
-					include_once 'templates/vision.php';
+					// include_once 'templates/vision.php';
 					include_once 'templates/home.php';
 					break;
 			}
