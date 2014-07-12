09e78ff585eddb2871785cca92742b26b7f7acdc
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-12 09:14:45 +0530

    Hiding the password warning from the google form

--- website/build/templates/membership.php
+++ website/build/templates/membership.php
@@ -45,6 +45,6 @@
 					</tr>
 				</tbody>
 			</table>
-			<iframe src="https://docs.google.com/forms/d/1Kmu-nHumR9uGZH55S9LkJXQA7B9sIHm5wNa5qFQMi4M/viewform?embedded=true" width="680" height="1850" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading...</iframe>
+			<iframe id="memberShipForm" src="https://docs.google.com/forms/d/1Kmu-nHumR9uGZH55S9LkJXQA7B9sIHm5wNa5qFQMi4M/viewform?embedded=true" width="680" height="1850" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading...</iframe>
 		</div>		
 	</div>
