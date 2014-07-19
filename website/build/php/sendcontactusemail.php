08bf20041c838cfaeca110fe25392582a6773b46
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-19 08:35:22 +0530

    Added more omages for banner

--- website/build/php/sendcontactusemail.php
+++ website/build/php/sendcontactusemail.php
@@ -8,7 +8,7 @@
 	$contactmessage = $_POST['message'];
 	
 	$mail_cfg['Fromname'] = $name;
-	$mail_cfg['From'] = 'reach@catt.org.in';
+	$mail_cfg['From'] = $emailaddr;
 	$mail_cfg['addaddress'] = 'reach@catt.org.in';
 	$mail_cfg['subject'] = "[CATT.ORG.IN] Contact Us [From: " . $emailaddr . "]";
 	$mail_cfg['body'] = "Name: $name\nEmail: $emailaddr\nMessage: $contactmessage\n\n";
--- website/build/templates/banner.php
+++ website/build/templates/banner.php
@@ -1,18 +1,29 @@
 		<div id="banner" class="w1000">
-			<img src="assets/images/banner/15072014/bnr-img-01.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-01.png" />
-			<img src="assets/images/banner/15072014/bnr-img-02.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-02.png" />
-			<img src="assets/images/banner/15072014/bnr-img-03.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-03.png" />
-			<img src="assets/images/banner/15072014/bnr-img-04.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-04.png" />
-			<img src="assets/images/banner/15072014/bnr-img-05.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-05.png" />
-			<img src="assets/images/banner/15072014/bnr-img-06.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-06.png" />
-			<img src="assets/images/banner/15072014/bnr-img-07.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-07.png" />
-			<img src="assets/images/banner/15072014/bnr-img-08.png" />
 			<img src="assets/images/banner/15072014/bnr-img-a-08.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-09.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-10.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-11.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-12.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-13.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-14.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-15.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-16.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-17.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-18.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-19.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-20.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-21.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-22.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-23.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-24.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-25.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-26.png" />
+			<img src="assets/images/banner/15072014/bnr-img-a-27.png" />
 		</div>
