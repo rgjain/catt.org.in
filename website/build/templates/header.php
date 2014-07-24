cbf03d286056d5040bfe4bd1409d1667b190bac6
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-24 07:41:21 +0530

    Merged the aboutus & team pages

--- website/build/templates/header.php
+++ website/build/templates/header.php
@@ -3,7 +3,6 @@
 			<a id="logo" href="./" title="Conservation Awareness Team Trust"><img src="assets/images/catt-logo-150.png" alt="CATT"></a>
 			<div id="nav">
 				<a href="./?p=aboutus" <?php if ($p == "aboutus") { echo 'class="active"';} ?> ><span>ABOUT US</span></a>
-				<a href="./?p=team" <?php if ($p == "team") { echo 'class="active"';} ?> ><span>TEAM</span></a>
 				<a href="./?p=events" <?php if ($p == "events") { echo 'class="active"';} ?> ><span>EVENTS</span></a>
 				<a href="./?p=gallery" <?php if ($p == "gallery") { echo 'class="active"';} ?> ><span>GALLERY</span></a>
 				<a href="./?p=membership" <?php if ($p == "membership") { echo 'class="active"';} ?> ><span>MEMBERSHIP</span></a>
