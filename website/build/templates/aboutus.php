e49d5e2cb02f2f305db89c23e8ca1519a7ac2b45
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-25 12:16:22 +0530

    Changes in the about us page

--- website/build/templates/aboutus.php
+++ website/build/templates/aboutus.php
@@ -1,13 +1,12 @@
 	<div id="aboutus" class="page w1000">
 		<h1>CATT - Conservation Awareness Team Trust</h1>
 		<div class="content">
-			<p>We, the members of the CATT family, are professionals from various industries and sectors, united by a singular passion: the love for our wildlife and the urge to do our bit to save what is left of our natural heritage.</p>
-			<p>We refuse to play mute spectators and sit idle, as our forests get decimated, wild animals get systematically eliminated, natural resources get exhausted and the environment gets polluted beyond repair. We refuse to indulge in irresponsible and unsustainable behavior that harms our environment and believe in educating others by spreading awareness about the same.</p>
+			<p>Over a debate of ecology versus economy, we often forget that both the words originate from the Greek term Oikos, meaning home. The relationship between ecology (study of our home environment) and economy (household resource management) is way more complex and inter-related than it seems to be, making it pivotal to draw a parallel between the two in order to reach at a sustainable future.We at CATT, do not consider nature and forests to be commodities that can be bought or sold and then converted to a tourist package, but as a sphere of life intertwining us with the wild. Here at CATT, WE BELIEVE IN CREATING A SAFE HAVEN FOR THE WILD.</p>
+			<p>The Conservation Awareness Team is a group of believers, who believes in the greater role the society as a whole and the community as one can play to bring about a much needed difference in this broken and often distributed populace of ours for the greater good of our nation?s majestic forests. It all started off with a small group of professionals coming together to share their common love for the wild which led to the inception of the Conservation Awareness Team Trust (CATT) as a not for profit organization.</p>
+			<p>We at CATT, believe that, even though multinational organizations exist for the conservation of wildlife, local players are crucial and must be predominant to bring about grass root level changes with the help of the community. CATT intends to be the voice of the jungle and preserve life within the forest ? the way it?s meant to be. The sole purpose of existence of CATT is to create a safe haven for the wild.</p>
 			<p>CATT is a platform for action.</p>
-			<p>What makes us effective is that our action isn't driven solely by emotions or misguided efforts. It is steered by conscious planning, thorough fieldwork and research, and leveraging a pool of diverse think tank powered by expertise, experience and willingness to embrace innovation. </p>
-			<p>We are determined to work at every level - from grass-root to administration, to identify specific problems faced by our natural environs, and actually implement, re-assess and refine the solutions.</p>
-			<p>There are many more like us, who want to do their bit and make our home a more beautiful place to live. CATT invites all such like-minded people to join hands and try to create a better planet. Together, we will make a difference.</p>
-			<p>The time is NOW! Everyone must act. Or it might be too late.</p>
+			<p>We are determined to work at every level - from grass-root to administration, to identify specific problems faced by our natural environs, and actually implement, re-assess and refine the solutions. There are many more like us, who want to do their bit and make our home a more beautiful place to live. CATT invites all such like-minded people to join hands and try to create a better planet.</p>
+			<p>Together, we will make a difference. The time is NOW! Everyone must act. Or it might be too late.</p>
 
 			<h2>THE TEAM</h2>
 			<div class="team-members">
