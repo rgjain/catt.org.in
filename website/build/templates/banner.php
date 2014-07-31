cff6055e11ec2fc7d0caf2cd1812a01b5ba054b4
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-07-31 15:45:32 +0530

    Adding the pagination to the banner

--- website/build/templates/banner.php
+++ website/build/templates/banner.php
@@ -1,12 +1,12 @@
 		<div id="banner" class="w1000">
-			<img src="assets/images/banner/15072014/bnr-img-a-01.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-02.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-03.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-04.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-05.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-06.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-07.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-08.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-09.png" />
-			<img src="assets/images/banner/15072014/bnr-img-a-10.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-01.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-02.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-03.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-04.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-05.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-06.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-07.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-08.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-09.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-10.png" />
 		</div>
