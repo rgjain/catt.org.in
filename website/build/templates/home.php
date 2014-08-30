f039b22230f1d94c3246af3a87ee227bbd30f934
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-08-30 21:42:13 +0530

    Upcoming event added

--- website/build/templates/home.php
+++ website/build/templates/home.php
@@ -7,19 +7,9 @@
 				</div>
 			</a>
 			<a class="section" href="./?p=upcomingevents">
-				<img src="assets/images/thumbs/thumb-2.png">
+				<img src="assets/images/thumbs/thumb-6.png">
 				<div class="detail">
 					<h2>UPCOMING EVENTS</h2>
-					<!-- <div id="event-info">
-						<h3>Wild India &amp; its Challenges</h3>
-						<strong>CATT - Workshop on 6th September, 2014</strong>
-						<p>at</p>
-						<strong>Atria Towers, Jain College Palace Road, Bangalore</strong>
-						<p>from</p>
-						<strong>10.30 AM - 17.00 PM IST</strong>
-						<p><big>Chief Guest &amp; Speaker: Jose Louise, Southern India Operation Lead, Wildlife Trust of India.</big></p>
-					</div> -->
-					<p>Follow us here for our upcoming events</p>
 				</div>
 			</a>
 			<a class="section" href="./?p=membership">
---
+++ website/build/templates/upcomingevents.php
@@ -0,0 +1,27 @@
+	<div id="upcomingevents" class="page w1000">
+		<h1>CATT's UPCOMING EVENTS</h1>
+		<div class="content">
+			<h2>Wild India &amp; its Challenges</h2>
+			<strong>CATT - Workshop on 6th September, 2014</strong>
+			<span>at</span>
+			<strong>Atria Towers, Jain College Palace Road, Bangalore</strong>
+			<span>from</span>
+			<strong>10.30 AM - 17.00 PM IST</strong>
+			<p><big>Chief Guest &amp; Speaker: Jose Louise, Southern India Operation Lead, Wildlife Trust of India.</big></p>
+
+			<p class="red"><strong>Note: </strong>Workshop is only for CATT Members.</p>
+
+			<h3>Detailed Agenda:</h3>
+			<ul>
+				<li>10:30  -  11:00 : CATT - Introduction</li>
+				<li>11:00  -  12:00 : Animal Behavior &amp; Challenges of Wildlife Conservation in India by Jose Louise</li>
+				<li>12.00  -  12.15 : Tea Break</li>
+				<li>12:15  -  13:00 : Understanding Snakes by Jose Louise</li>
+				<li>13:00  -  14:00 : Lunch Break</li>
+				<li>14:00  -  15:00 : CATT Members Introduction</li>
+				<li>15:00  -  16:00 : Wildlife Documentary "Truth About Tigers" by Shekhar Dattatri.</li>
+				<li>16:00  -  16:45 : Question &amp; Answer Session for CATT Members by Sayak Dutta.</li>
+				<li>16.45  -  17.00 : Tea &amp; Closure.</li>
+			</ul>
+		</div>
+	</div>
