875353b92d787cddcca1f69ff8d14f62ab201a54
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-09-03 07:27:19 +0530

    Members page changed

--- website/build/templates/members.php
+++ website/build/templates/members.php
@@ -3,216 +3,161 @@
 		<div class="content">
 			<p class="intro">We are really grateful to all our CATT members. Our members have helped us to grow by providing financial support as well as volunteering in several field &amp; city based events. Most of our members are professionals in the corporate world but all of them share a common interest to conserve nature &amp; wildlife. CATT members comprise of wildlife enthusiasts, nature lovers, trekkers, amature photographers, birders and mountainers. Read more about few of our most active and vibrant Members.
 			<a href="./?p=membership">Click here to become a member</a></p>
-			<dl>
-				<dt>
+			<ul>
+				<li>
+					<img src="assets/images/profile/members/sureshbabu.png" alt="Suresh Babu">
 					<h2>Suresh Babu</h2>
-				</dt>
-				<dd class="clearfix">
 					<div>
 						By profession a prominent businessman, by passion a wildlife enthusiast is one of the most active volunteers for CATT. He volunteered in Litter Clearing Drive 4 &amp; 5, Foot Patrolling 1 and Field support in Bandipur national park. He has also attended a Workshop on Eradication of Invasive Weeds and Habitat Restoration in Bandipur Tiger Reserve. Suresh was part of Tiger Census Phase 1 conducted by Karnataka Forest Department and assigned to Kudremukha National Park.
 					</div>
-					<img src="assets/images/profile/members/sureshbabu.png" alt="Suresh Babu">
-				</dd>
-
-				<dt>
-					<h2>Deepak Shankar </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/deepakshankar.png" alt="Deepak Shankar">
+					<h2>Deepak Shankar</h2>
 					<div>
 						Deepak is a financial analyst, a wildlife enthusiast and a Managing Committee member of Banerghatta Rehabilitation Center (BRC), Bangalore. He was one of the volunteers for CATT's first field activity i.e. Forest Fire Awareness Programme in Bandipur Tiger Reserve. He participated in Tiger Census Phase 1 conducted by Karnataka Forest Department and was assigned to Kudremukha National Park.
 					</div>
-					<img src="assets/images/profile/members/deepakshankar.png" alt="Deepak Shankar">
-				</dd>
-
-				<dt>
+				</li>
+				<li>
+					<img src="assets/images/profile/members/shyamal.png" alt="Shyamal Majumder">
 					<h2>Shyamal Majumder</h2>
-				</dt>	
-				<dd class="clearfix">
 					<div>
 						A software professional Shyamal is an avid trekker, rock climber and a wildlife enthusiast. He is the first member of CATT and volunteered in CATT's third Litter Clearing Drive which was carried out at Talacauvery Wildlife Sanctuary, Coorg.
 					</div>
-					<img src="assets/images/profile/members/shyamal.png" alt="Shyamal Majumder">
-				</dd>
-
-				<dt>
+				</li>
+				<li>
+					<img src="assets/images/profile/members/achintya.png" alt="Achintya Kundu">
 					<h2>Achintya Kundu</h2>
-				</dt>	
-				<dd class="clearfix">
 					<div>
 						Achintya is a student in IISC, Bangalore and an avid trekker. He draws attention into wildlife conservation from the experiences while trekking in different part of Western Ghat. He volunteered in CATT's third Litter Clearing Drive and field support in Bandipur Tiger Reserve.
 					</div>
-					<img src="assets/images/profile/members/achintya.png" alt="Achintya Kundu">
-				</dd>
-
-				<dt>
-					<h2>Govind Gorur </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/govindgorur.png" alt="Govind Gorur">
+					<h2>Govind Gorur</h2>
 					<div>
 						Business Analyst by profession, Govind is an amature photographer, a birder and an avid trekker. He has been roaming around Himalayas and jungles &amp; hills of Western Ghat for several years. This drove him to contribute towards wildlife conservation. He volunteered in field support in Bandipur Tiger Reserve. He participated in Tiger Census Phase 1 conducted by Karnataka Forest Department and was assigned to Kudremukha National Park.
 					</div>
-					<img src="assets/images/profile/members/govindgorur.png" alt="Govind Gorur">
-				</dd>
-
-				<dt>
-					<h2>Girish K </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/girish.png" alt="Girish K">
+					<h2>Girish K</h2>
 					<div>
 						Girish, a techie by profession but a passionate birder, an amature photographer and an expert mountaineer is one of the self motivated and most active member of CATT. He volunteered in field support in Bandipur Tiger Reserve. He contributed lot of photographs towards CATT website.
 					</div>
-					<img src="assets/images/profile/members/girish.png" alt="Girish K">
-				</dd>
-
-				<dt>
-					<h2>Vinayak Krishnamurthy </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/vinayak.png" alt="Vinayak Krishnamurthy">
+					<h2>Vinayak Krishnamurthy</h2>
 					<div>
 						One of our most active Member. He has participated in multiple field &amp; city based activities. He has helped to enhance CATT Team.
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/vinayak.png" alt="Vinayak Krishnamurthy">
-				</dd>
-
-				<dt>
-					<h2>Sriram Prabhakar </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Sriram Prabhakar</h2>
 					<div>
 						Sriram by profession a HR expert and by passions a wildlife enthusiast, is one of the most prominent volunteers for CATT. He has participated in one of the Eco treks and volunteered in CATT's first Litter Clearing Drive in Bandipur Tiger Reserve.
 					</div>
-				</dd>
-
-				<dt>
-					<h2>Ananda Veluswamy </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/anandakumar.png" alt="Ananda Velluswamy">
+					<h2>Ananda Veluswamy</h2>
 					<div>
 						Ananda is an avid Trekker with lot of wildlife enthusiasm. He helped us in conducting awareness campaign during VIBHA Dream Mile Run.
 					</div>
-					<img src="assets/images/profile/members/anandakumar.png" alt="Ananda Velluswamy">
-				</dd>
-
-				<dt>
-					<h2>Rojin V </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/rojin.png" alt="Rojin">
+					<h2>Rojin V</h2>
 					<div>
 						He is an IT Specialist with love of Nature photography. Rojin has helped us in Tree plantation activity with Vikas Team.
 					</div>
-					<img src="assets/images/profile/members/rojin.png" alt="Rojin">
-				</dd>
-
-				<dt>
-					<h2>Vijay Kulkarni </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/vijaykulkarni.png" alt="Vijay Kulkarni ">
+					<h2>Vijay Kulkarni</h2>
 					<div>
 						He works for an IT Major but has avid passion for protection of wildlife. He has participated in multiple field activities &amp; special mention he helped us to complete Medical Camp successfully.
 					</div>
-					<img src="assets/images/profile/members/vijaykulkarni.png" alt="Vijay Kulkarni ">
-				</dd>
-
-				<dt>
-					<h2>Gagan Gowda </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/gagangowda.png" alt="Rojin">
+					<h2>Gagan Gowda</h2>
 					<div>
 						A Student who loves wild &amp; full of enthusiasm. He is an active member of CATT &amp; participates in various activities.
 					</div>
-					<img src="assets/images/profile/members/gagangowda.png" alt="Rojin">
-				</dd>
-
-				<dt>
-					<h2>Sreekanta </h2>
-				</dt>	
-				<dd class="clearfix">
+				</li>
+				<li>
+					<img src="assets/images/profile/members/sreekanta.png" alt="Sreekanta">
+					<h2>Sreekanta</h2>
 					<div>
 						A native of Bandipur our real hero who ardent birder &amp; have been instrumental in our multiple field activities.
 					</div>
-					<img src="assets/images/profile/members/sreekanta.png" alt="Sreekanta">
-				</dd>
-
-				<dt>
-					<h2>Amit Kayal </h2>
-				</dt>	
-				<dd class="clearfix">
-					<div>
-					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/amitkayal.png" alt="Amit Kayal">
-				</dd>
-
-				<dt>
-					<h2>Devleena Bhattacharya </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Amit Kayal</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/devleenabhattacharya.png" alt="Devleena Bhattacharya">
-				</dd>
-
-				<dt>
-					<h2>Kunal </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Devleena Bhattacharya</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/kunal.png" alt="Kunal">
-				</dd>
-
-				<dt>
-					<h2>Pankaj Jain </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Kunal</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/pankajjain.png" alt="Pankaj Jain">
-				</dd>
-
-				<dt>
-					<h2>Sujata Nanda </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Pankaj Jain</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/sujatananda.png" alt="Sujata Nanda">
-				</dd>
-
-				<dt>
-					<h2>Nirmalya Chakraborty </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Sujata Nanda</h2>
 					<div>
 					</div>
+					
+				</li>
+				<li>
 					<img src="assets/images/profile/members/nirmalyachakraborty.png" alt="Nirmalya Chakraborty">
-				</dd>
-
-				<dt>
-					<h2>Girish Gumbalapuram </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Nirmalya Chakraborty</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/girishgumbalapuram.png" alt="Girish Gumbalapuram">
-				</dd>
-
-				<dt>
-					<h2>Praveen Byrappa </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Girish Gumbalapuram</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/praveenbyrappa.png" alt="Praveen Byrappa">
-				</dd>
-
-				<dt>
-					<h2>Mukesh Prabhu </h2>
-				</dt>	
-				<dd class="clearfix">
+					<h2>Praveen Byrappa</h2>
 					<div>
+					
 					</div>
+				</li>
+				<li>
 					<img src="assets/images/profile/members/mukeshprabhu.png" alt="Mukesh Prabhu">
-				</dd>
-			</dl>
+					<h2>Mukesh Prabhu</h2>
+					<div>
+					
+					</div>
+				</li>
+			</ul>
 		</div>		
 	</div>
