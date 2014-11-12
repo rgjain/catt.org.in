0c3d7163a17738c08f087831f44186338cadb64c
Rupesh Jain <rupeshkumar.rj@gmail.com>
2014-11-12 07:09:36 +0530

    Fixed the membership form issue

--- website/build/templates/membershipform.php
+++ website/build/templates/membershipform.php
@@ -1,6 +1,6 @@
 	<div id="membershipform" class="page w1000">
 		<h1>MEMBERSHIP FORM</h1>
 		<div class="content">
-			<iframe id="memberShipForm" src="https://docs.google.com/forms/d/1Kmu-nHumR9uGZH55S9LkJXQA7B9sIHm5wNa5qFQMi4M/viewform?embedded=true" width="680" height="1820" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading...</iframe>
+			<iframe id="memberShipForm" src="https://docs.google.com/forms/d/1Kmu-nHumR9uGZH55S9LkJXQA7B9sIHm5wNa5qFQMi4M/viewform?embedded=true" width="680" height="2035" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading...</iframe>
 		</div>		
 	</div>
