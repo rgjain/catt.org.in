d23542478e808c0479701de149f4f128942f1287
Rupesh Jain <rupeshkumar.rj@gmail.com>
2015-06-17 22:17:05 +0530

    Adding the social links

--- website/build/templates/footer.php
+++ website/build/templates/footer.php
@@ -1,4 +1,16 @@
 		<div id="footer">
-			<p><a href="./">CATT - Conservation Awareness Team Trust - ESTD: 01-01-2014</a></p>
-			<p>#018, A Block, Adithya Soigne Apartment, BEML Road, Shivanandanagar, Bangalore -560075</p>
+			<div class="w1000 clearfix">
+				<div class="footer-left" >
+					<p><a href="./">CATT - Conservation Awareness Team Trust - ESTD: 01-01-2014</a></p>
+					<p>#018, A Block, Adithya Soigne Apartment, BEML Road, Shivanandanagar, Bangalore -560075</p>
+				</div>
+				<div class="footer-right" >
+					<a class="links" href="https://www.facebook.com/CATTConservationAwarenessTeamTrust" title="connect with us on facebook">
+						<img src="assets/images/FBLogoblue29.png" alt="connect with us on facebook" title="connect with us on facebook" />
+					</a>
+					<a class="links" href="https://twitter.com/catt_org" title="connect with us on twitter">
+						<img src="assets/images/TwitterLogo29.png" alt="connect with us on twitter" title="connect with us on twitter" />
+					</a>
+				</div>
+			</div>
 		</div>
