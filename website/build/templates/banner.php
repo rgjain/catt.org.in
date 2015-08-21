fecdc42569ed86a10401b40f771639673d0ae53d
Rupesh Jain <rupeshkumar.rj@gmail.com>
2015-08-21 11:09:46 +0530

    Converted PNG to JPG

--- website/build/templates/banner.php
+++ website/build/templates/banner.php
@@ -1,12 +1,12 @@
 		<div id="banner" class="w1000">
-			<img src="assets/images/banner/w1000x500/bnr-img-a-02.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-04.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-06.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-07.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-28.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-29.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-30.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-31.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-32.png" />
-			<img src="assets/images/banner/w1000x500/bnr-img-a-33.png" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-02.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-04.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-06.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-07.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-28.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-29.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-30.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-31.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-32.jpg" />
+			<img src="assets/images/banner/w1000x500/bnr-img-a-33.jpg" />
 		</div>
