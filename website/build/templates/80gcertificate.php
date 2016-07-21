7a7685c3247ece7fc1fbbe0955e84024ca23fdcc
Rupesh Jain <rupeshkumar.rj@gmail.com>
2016-07-21 02:47:54 +0100

    80 G Certificate

---
+++ website/build/templates/80gcertificate.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<style type="text/css">
+		body {
+			font-family: Arial, sans-serif;
+			font-size: 13px;
+			width: 1000px;
+			margin: 0 auto;
+		}
+		p {
+			margin: 0.5em 0;
+		}
+		th {
+			text-align: left;
+		}
+		td, th {
+			padding: 5px 0;
+		}
+	</style>
+</head>
+<body>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tbody>
+			<tr>
+				<td colspan="2" align="center">
+					<img src="http://catt.org.in/assets/images/catt-logo-150.png">
+					<h1>CATT - Conservation Awareness Team Trust</h1>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2"><hr></td>
+			</tr>
+			<tr>
+				<td>
+					<p>#018, A Block, Adithya Soigne Apartment,</p>
+					<p>BEML Road, Shivanandanagar, Bangalore -560075</p>
+					<p>Website: www.catt.org.in<a href="http://www.catt.org.in"></a></p>
+					<p>Email: <a href="mailto:reach@catt.org.in">reach@catt.org.in</a></p>
+				</td>
+				<td align="right">
+					<p>Registration No. AS/3454FFF</p>
+					<p>PAN: AXXPX0000N</p>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2"><hr></td>
+			</tr>
+		</tbody>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tbody>
+			<tr>
+				<td>Donor ID: CATT/324234</td>
+				<td align="right">Receipt No: CATT/2016/34234</td>
+			</tr>
+			<tr>
+				<td>Receipt Date: 20-Jul-2016</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td colspan="2"><hr></td>
+			</tr>
+		</tbody>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tbody>
+			<tr>
+				<td>Received with thanks from <strong>MR. SARADHI ROY</strong></td>
+			</tr>
+			<tr>
+				<td>
+					<p>018, A BLOCK, ADITHYA SOIGNE, BEML ROAD, SHIVANANDANAGAR </p>
+					<p>BANGALORE, KARNATAKA</p>
+					<p>Pin :560075 (INDIA)</p>
+				</td>
+				<tr>
+					<td><hr></td>
+				</tr>
+			</tr>
+		</tbody>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<thead>
+			<tr>
+				<th>Purpose of Donation</th>
+				<th>Mode of Payment</th>
+				<th>Reference No.</th>
+				<th>Amount (INR)</th>
+			</tr>
+			<tr>
+				<th colspan="4"><hr></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>LPG Connection Donation Drive</td>
+				<td>NEFT</td>
+				<td>344534636</td>
+				<td>3000.00</td>
+			</tr>
+			<tr>
+				<td>Medical Camp</td>
+				<td>Cash</td>
+				<td>&nbsp;</td>
+				<td>5000.00</td>
+			</tr>
+			<tr>
+				<td>Total Amount</td>
+				<td>&nbsp;</td>
+				<td>&nbsp;</td>
+				<td>8000.00</td>
+			</tr>
+		</tbody>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tbody>
+			<tr>
+				<td><hr></td>
+			</tr>
+			<tr>
+				<td><strong>In words: </strong> INR: Eight thousand only</td>
+			</tr>
+			<tr>
+				<td><hr></td>
+			</tr>
+		</tbody>
+	</table>
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+		<thead>
+			<tr>
+				<th>This is a computer generated receipt. Signature not required.</td>
+			</tr>
+		</thead>
+	</table>
+</body>
+</html>
