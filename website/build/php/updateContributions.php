cc3d313a5a5741eb851eab57a3f7e0344891a304
Rupesh Jain <rupeshkumar.rj@gmail.com>
2017-01-13 21:14:18 +0530

    Implemented the amount in words instead of NumberFormatter class

--- website/build/php/updateContributions.php
+++ website/build/php/updateContributions.php
@@ -21,8 +21,7 @@
 
 	        list ($name, $email_address, $address, $amount, $cause, $id, $timestamp, $date, $status, $transref, $eightygcert_id, $payment_mode) = mysql_fetch_row($getres);
 
-			$numformat = new NumberFormatter("en", NumberFormatter::SPELLOUT);
-			$amount_in_words = $numformat->formatCurrency ($amount, "INR");
+			$amount_in_words = amountInWords($amount);
 
 	        ob_start();
 	        include_once("contributionEmail.php");
@@ -64,4 +63,74 @@
 // client ID - 369320507427-59khundtso4otj1p3u52a7mri0oc5k8f.apps.googleusercontent.com
 // client secret - gOLGpBd_TJzD_9N-ilfIwrkK
 
+
+	/**
+	* Created by PhpStorm.
+	* User: sakthikarthi
+	* Date: 9/22/14
+	* Time: 11:26 AM
+	* Converting Currency Numbers to words currency format
+	*/
+	function amountInWords($number) {
+		// $number   = 190908100.25;
+		$no       = round($number);
+		$point    = round($number - $no, 2) * 100;
+		$hundred  = null;
+		$digits_1 = strlen($no);
+		$i        = 0;
+		$str      = array();
+		$words    = array(
+			'0' => '',
+			'1' => 'one',
+			'2' => 'two',
+			'3' => 'three',
+			'4' => 'four',
+			'5' => 'five',
+			'6' => 'six',
+			'7' => 'seven',
+			'8' => 'eight',
+			'9' => 'nine',
+			'10' => 'ten',
+			'11' => 'eleven',
+			'12' => 'twelve',
+			'13' => 'thirteen',
+			'14' => 'fourteen',
+			'15' => 'fifteen',
+			'16' => 'sixteen',
+			'17' => 'seventeen',
+			'18' => 'eighteen',
+			'19' => 'nineteen',
+			'20' => 'twenty',
+			'30' => 'thirty',
+			'40' => 'forty',
+			'50' => 'fifty',
+			'60' => 'sixty',
+			'70' => 'seventy',
+			'80' => 'eighty',
+			'90' => 'ninety'
+		);
+		$digits   = array(
+			'',
+			'hundred',
+			'thousand',
+			'lakh',
+			'crore'
+		);
+		while ($i < $digits_1) {
+			$divider = ($i == 2) ? 10 : 100;
+			$number  = floor($no % $divider);
+			$no      = floor($no / $divider);
+			$i += ($divider == 10) ? 1 : 2;
+			if ($number) {
+				$plural  = (($counter = count($str)) && $number > 9) ? 's' : null;
+				$hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+				$str[]   = ($number < 21) ? $words[$number] . " " . $digits[$counter] . $plural . " " . $hundred : $words[floor($number / 10) * 10] . " " . $words[$number % 10] . " " . $digits[$counter] . $plural . " " . $hundred;
+			} else
+			$str[] = null;
+		}
+		$str    = array_reverse($str);
+		$result = implode('', $str);
+		// $points = ($point) ? "." . $words[$point / 10] . " " . $words[$point = $point % 10] : '';
+		return $result;
+	}
 ?>
