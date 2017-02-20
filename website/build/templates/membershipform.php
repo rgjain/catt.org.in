acf81ce85c203502147a7a0963a6a0c75a3ce078
Rupesh Jain <rupeshkumar.rj@gmail.com>
2017-02-20 21:30:44 +0530

    Restoring the membership google form

--- website/build/templates/membershipform.php
+++ website/build/templates/membershipform.php
@@ -1,6 +1,6 @@
 	<div id="membershipform" class="page w1000">
 		<h1>MEMBERSHIP FORM</h1>
 		<div class="content">
-			<iframe id="memberShipForm" src="https://docs.google.com/forms/d/1Kmu-nHumR9uGZH55S9LkJXQA7B9sIHm5wNa5qFQMi4M/viewform?embedded=true" width="680" height="2035" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading...</iframe>
+			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc1b8_9hfD7Wm617XtEmgYqzJyP61iDK9oB1W4o8NFcXtiHFg/viewform?embedded=true" width="680" height="2035" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 		</div>		
 	</div>
